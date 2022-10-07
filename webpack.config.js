b82ce298999d035280ca2ced0da531931be9669e
mvchn <movchan@gmail.com>
2022-10-07 18:00:34 +0300

    add production mode

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/js/main.js',
+    plugins: [new miniCssExtractPlugin()],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Extracts CSS for each JS file that includes CSS
+                        loader: miniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: () => [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}
